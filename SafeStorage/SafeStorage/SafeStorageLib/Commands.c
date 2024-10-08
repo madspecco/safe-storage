412f2c63215540b4d1d296f6f696d670e28e3cb7
Bogdan-Sergiu Boloș <102137035+madspecco@users.noreply.github.com>
2024-10-08 10:44:18 +0300

    Initial commit

---
+++ SafeStorage/SafeStorage/SafeStorageLib/Commands.c
@@ -0,0 +1,120 @@
+﻿#include "Commands.h"
+
+
+NTSTATUS WINAPI
+SafeStorageInit(
+    VOID
+)
+{
+    /* The function is not implemented. It is your responsibility. */
+    /* Here you can create any global objects you consider necessary. */
+
+    return STATUS_SUCCESS;
+}
+
+
+VOID WINAPI
+SafeStorageDeinit(
+    VOID
+)
+{
+    /* The function is not implemented. It is your responsibility. */
+    /* Here you can clean up any global objects you have created earlier. */
+
+    return;
+}
+
+
+NTSTATUS WINAPI
+SafeStorageHandleRegister(
+    const char* Username,
+    uint16_t UsernameLength,
+    const char* Password,
+    uint16_t PasswordLength
+)
+{
+    /* The function is not implemented. It is your responsibility. */
+    /* After you implement the function, you can remove UNREFERENCED_PARAMETER(x). */
+    /* This is just to prevent a compilation warning that the parameter is unused. */
+
+    UNREFERENCED_PARAMETER(Username);
+    UNREFERENCED_PARAMETER(UsernameLength);
+    UNREFERENCED_PARAMETER(Password);
+    UNREFERENCED_PARAMETER(PasswordLength);
+
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+
+NTSTATUS WINAPI
+SafeStorageHandleLogin(
+    const char* Username,
+    uint16_t UsernameLength,
+    const char* Password,
+    uint16_t PasswordLength
+)
+{
+    /* The function is not implemented. It is your responsibility. */
+    /* After you implement the function, you can remove UNREFERENCED_PARAMETER(x). */
+    /* This is just to prevent a compilation warning that the parameter is unused. */
+
+    UNREFERENCED_PARAMETER(Username);
+    UNREFERENCED_PARAMETER(UsernameLength);
+    UNREFERENCED_PARAMETER(Password);
+    UNREFERENCED_PARAMETER(PasswordLength);
+
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+
+NTSTATUS WINAPI
+SafeStorageHandleLogout(
+    VOID
+)
+{
+    /* The function is not implemented. It is your responsibility. */
+
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+
+NTSTATUS WINAPI
+SafeStorageHandleStore(
+    const char* SubmissionName,
+    uint16_t SubmissionNameLength,
+    const char* SourceFilePath,
+    uint16_t SourceFilePathLength
+)
+{
+    /* The function is not implemented. It is your responsibility. */
+    /* After you implement the function, you can remove UNREFERENCED_PARAMETER(x). */
+    /* This is just to prevent a compilation warning that the parameter is unused. */
+
+    UNREFERENCED_PARAMETER(SubmissionName);
+    UNREFERENCED_PARAMETER(SubmissionNameLength);
+    UNREFERENCED_PARAMETER(SourceFilePath);
+    UNREFERENCED_PARAMETER(SourceFilePathLength);
+
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+
+NTSTATUS WINAPI
+SafeStorageHandleRetrieve(
+    const char* SubmissionName,
+    uint16_t SubmissionNameLength,
+    const char* DestinationFilePath,
+    uint16_t DestinationFilePathLength
+)
+{
+    /* The function is not implemented. It is your responsibility. */
+    /* After you implement the function, you can remove UNREFERENCED_PARAMETER(x). */
+    /* This is just to prevent a compilation warning that the parameter is unused. */
+
+    UNREFERENCED_PARAMETER(SubmissionName);
+    UNREFERENCED_PARAMETER(SubmissionNameLength);
+    UNREFERENCED_PARAMETER(DestinationFilePath);
+    UNREFERENCED_PARAMETER(DestinationFilePathLength);
+
+    return STATUS_NOT_IMPLEMENTED;
+}
